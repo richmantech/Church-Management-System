b8195c60fac1ca4c56d49bf35ec37f63a9ae4bff
richmantech <105031757+richmantech@users.noreply.github.com>
2022-07-23 22:13:36 -0700

    Create delete_modal.php

---
+++ delete_modal.php
@@ -0,0 +1,35 @@
+
+
+
+<div id="myModal" class="modal fade" role="dialog">
+	
+	<div class="modal-dialog">
+		
+		<div class="modal-content">
+			
+			<div class="modal-header">
+				
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				
+			</div>
+			<div class="modal-body">
+				
+				<h4>Are Sure You Want To Delete</h4>
+				
+			</div>
+			<div class="modal-footer">
+				<a class="btn btn-danger modal_delete_link">Delete</a>
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+				
+			</div>
+		</div>
+		
+	</div>
+	
+	
+	
+	
+	
+	
+	
+</div>
